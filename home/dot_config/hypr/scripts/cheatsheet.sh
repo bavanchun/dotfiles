f247961b049a5ffe52e8dabf9fdff044c7f3a142
VChun <hoangbavan4478@gmail.com>
2026-04-14 10:45:36 +0700

    feat(hypr): add fuzzy search to cheatsheet via fzf

--- home/dot_config/hypr/scripts/cheatsheet.sh
+++ home/dot_config/hypr/scripts/cheatsheet.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 CHEATSHEET="$HOME/.config/hypr/cheatsheet.md"
 
-kitty --class cheatsheet --title "Cheatsheet" -e glow --style dark --pager "$CHEATSHEET"
+kitty --class cheatsheet --title "Cheatsheet" -e bash -c \
+    "glow --style dark --no-pager '$CHEATSHEET' | fzf --ansi --no-sort --reverse \
+        --prompt '  Search: ' --header 'Press ESC or q to close' \
+        --bind 'esc:abort,q:abort'"
